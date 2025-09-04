94d0a602a84ad85a7108d5d829c32c3f6db2cc5a
Mehmet Çalıkoğlu <memocaldev@gmail.com>
2025-09-05 02:21:17 +0300

    add star background and light/dark mode toggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,19 @@
+import { Star } from "lucide-react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
 };
